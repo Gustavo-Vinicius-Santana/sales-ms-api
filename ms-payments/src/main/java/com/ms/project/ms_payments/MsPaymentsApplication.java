d17919731b9315491669167dbcd722133a22860a
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-11-09 22:37:27 -0300

    (adjust): Adding Docker to MS Payments

--- ms-payments/src/main/java/com/ms/project/ms_payments/MsPaymentsApplication.java
+++ ms-payments/src/main/java/com/ms/project/ms_payments/MsPaymentsApplication.java
@@ -2,12 +2,15 @@ package com.ms.project.ms_payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.ms.project.ms_payments.repository")
+@EntityScan(basePackages = "com.ms.project.ms_payments.model")
 public class MsPaymentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsPaymentsApplication.class, args);
 	}
-
 }
